25cf255ff1fa662a73b50b5fc65746a65b558cb3
Jr Bytes <itamazor@gmail.com>
2022-10-13 18:25:05 -0300

    config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-export function App() {
-  return <h1>Hello world!</h1>
-}
+import './styles/global.css'
 
-export default App
+export function App() {
+  return (
+    <>
+      <h1 className='font-bold text-2xl text-violet-500'>Hello world!</h1>
+
+      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+        Enviar
+      </button>
+    </>
+  )
+}
